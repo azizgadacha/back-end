076b6ba0ebb8fcb4ce51295ebb9755a3625556c6
Ouessim <aboucha.ouessim123@gmail.com>
2022-04-05 16:21:20 +0100

    delete user cascade

--- src/controller/DeleteUser.js
+++ src/controller/DeleteUser.js
@@ -7,38 +7,49 @@ const workspace = require("../model/workspace");
 exports.DeleteUser=async (req, res) => {
 
     let id = req.body.user_id;
-console.log(id)
-    if(User.findOne({_id:id}))
-    {
+    console.log(id)
+    if (User.findOne({_id: id})) {
         var descendants = []
         var workspaceitems = []
-        var stack = [];
-        var item = await workspace.findOne({superior_id: id});
-        console.log('111111111111111')
-        console.log(item)
-        stack.push(item);
-        workspaceitems.push(item)
-        descendants.push(item._id)
-        while (stack.length > 0) {
 
-            var currentnode = stack.pop();
-            var children = await workspace.find({superior_id: currentnode._id});
-            console.log(children)
-            children.forEach(function (child) {
-                descendants.push(child._id);
-                workspaceitems.push(child)
-                stack.push(child);
-            });
+        //descendants.push(id)
+        var items = await workspace.find({superior_id: id})
+        console.log(items)
+        for (let item of items) {
+            descendants.push(item._id)
+            console.log('111111111111111')
+            console.log(item)
+            var stack = [];
+            stack.push(item);
+            workspaceitems.push(item)
+            while (stack.length > 0) {
+                var currentnode = stack.pop();
+                var children = await workspace.find({superior_id: currentnode._id});
+                console.log(children)
+                children.forEach(function (child) {
+                    descendants.push(child._id);
+                    workspaceitems.push(child)
+                    stack.push(child);
+                });
+            }
+            descendants.join(",")
+            for(item of descendants){
+                console.log(item.toString())
+                await workspace.findByIdAndRemove(item.toString())
+
+            }
+
+
+
+
 
         }
-        descendants.join(",")
-        for (item of descendants) {
-            console.log(item.toString())
-            await workspace.findByIdAndRemove(item.toString())
+        console.log(workspaceitems)
 
-        }
     }
-    console.log(workspaceitems)
+
+
+
     User.findOneAndDelete({_id:id}).then((user) => {
         console.log(user)
 
@@ -57,4 +68,11 @@ console.log("hello"+user.photo)
             res.json({success: false, msg: "error  user dosn't excite "});
         }
 
+
+
     })}
+
+
+
+
+
