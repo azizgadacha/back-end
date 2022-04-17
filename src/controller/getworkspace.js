1204469d7b944ee50acbe2d45131e4a806500593
aziz gadacha <aziz.gadacha.2000@gmail.com>
2022-04-17 17:43:49 +0100

    after catastrophe  tawa narja3

--- src/controller/getworkspace.js
+++ src/controller/getworkspace.js
@@ -1,21 +1,50 @@
 const workspace =require('../model/workspace')
 const activeSession =require('../model/activeSession')
 exports.getworkspace= (req, res,next) => {
-    const superior_id= String(req.body.superior_id);
+
+    let {list} = req.body
+    let exist = true
+    for (let elem of list) {
+
+        workspace.findOne({_id: elem})
+            .then((worksp) => {
+
+                    if (!(worksp)) {
+
+                        exist = false
+                        res.json({success: false,Valid:false});
+
+                        break;
+
+                    } else {
+                        if (worksp._id = list[i]) {
+
+                            workspace.find({superior_id:superior_id})
+                                .then((workspaceitems)=>{
+
+                                    res.json({success: true, workspaceitems});
+
+                                })
+                                .catch(() => res.json({ success: false }));
 
 
 
-            workspace.find({superior_id:superior_id})
-                .then((workspaceitems)=>{
-
-  res.json({success: true, workspaceitems});
 
 
 
+                        }
 
 
-                })
-                .catch(() => res.json({ success: false }));
-        }
+                    }
+
+                }
+            )
+    }
+
+
+}
+
+
+
 
 
