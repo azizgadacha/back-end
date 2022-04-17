c8507baaaaf3aff729a26a5bc60291965ed4bf02
Ouessim <aboucha.ouessim123@gmail.com>
2022-04-17 14:43:32 +0100

    Share Space and get this Shared space is done

--- src/controller/getSharedWorkspaces.js
+++ src/controller/getSharedWorkspaces.js
@@ -5,8 +5,6 @@ exports.getSharedWorkspace=  async (req, res,next) => {
     var item =await workspace.find({});
     for(let item1 of item){
         for(let i of item1.Share) {
-            console.log(i)
-            console.log(id)
             if (id === i) {
                 workspaceitems.push(item1)
             }
