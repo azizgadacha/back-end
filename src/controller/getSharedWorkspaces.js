ae50361a013d7275f66dd45863934f45c9a3d558
Ouessim <aboucha.ouessim123@gmail.com>
2022-04-17 05:12:09 +0100

    t3ebet space

--- src/controller/getSharedWorkspaces.js
+++ src/controller/getSharedWorkspaces.js
@@ -1,16 +1,16 @@
 const workspace =require('../model/workspace')
 exports.getSharedWorkspace=  async (req, res,next) => {
     var  id = req.body.user_id;
-    var list=[]
+    var workspaceitems=[]
     var item =await workspace.find({});
     for(let item1 of item){
         for(let i of item1.Share) {
             console.log(i)
             console.log(id)
             if (id === i) {
-                list.push(item1._id)
+                workspaceitems.push(item1)
             }
         }
     }
-    res.json({success: true, list})
+    res.json({success: true, workspaceitems})
 }
