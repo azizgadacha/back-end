08a1b5efe79714d07aa35617054c0cd6920f39e8
aziz gadacha <aziz.gadacha.2000@gmail.com>
2022-04-28 04:49:55 +0100

    salah

--- src/function/functionData/editWidgetlink.js
+++ src/function/functionData/editWidgetlink.js
@@ -4,11 +4,9 @@ const data =require( '../../model/data');
 
 
 exports.editWidgetlink=(req, res) => {
-console.log("salem")
 
     const { idData,newName,superiorID,type,WidgetName } = req.body;
     let  mg={ idData,newName,superiorID,type,WidgetName }
-    console.log(mg)
 
     data.findOneAndUpdate({ _id: idData ,usedIn:{superiorID,type,WidgetName}},{usedIn:{superiorID,type,WidgetName:newName}}).then((dataUpdated) => {
         if (dataUpdated){
