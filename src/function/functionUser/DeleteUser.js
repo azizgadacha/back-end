c3fee5ea2a8ea5595ead7b3ae90235accc3f4a14
aziz gadacha <aziz.gadacha.2000@.com>
2022-05-25 17:03:34 +0100

    finition9

--- src/function/functionUser/DeleteUser.js
+++ src/function/functionUser/DeleteUser.js
@@ -7,52 +7,47 @@ const activeSession=require('../../model/activeSession')
 exports.DeleteUser=async (req, res) => {
 
     let id = req.body.user_id;
-
-    if (User.findOne({_id: id})) {
-        var descendants = []
-        var workspaceitems = []
-
-        //descendants.push(id)
-        var items = await workspace.find({superior_id: id})
-        for (let item of items) {
-            descendants.push(item._id)
-
-            var stack = [];
-            stack.push(item);
-            workspaceitems.push(item)
-            while (stack.length > 0) {
-                var currentnode = stack.pop();
-                var children = await workspace.find({superior_id: currentnode._id});
-                children.forEach(function (child) {
-                    descendants.push(child._id);
-                    workspaceitems.push(child)
-                    stack.push(child);
-                });
-            }
-            descendants.join(",")
-            for (item of descendants) {
-                await workspace.findByIdAndRemove(item.toString())
-
-            }
-
-
-        }
-        activeSession.deleteMany({userId:id})
-            .then((AS)=>{
-            console.log("ActiveSession deleted Succesufully")
-                console.log(AS)
-            })
-
-
-
-
-    }
-
-
-    User.findOneAndDelete({_id: id}).then((user) => {
-
+    User.findOneAndDelete({_id: id}).then(async (user) => {
 
         if (user) {
+            var descendants = []
+            var workspaceitems = []
+
+            //descendants.push(id)
+            var items = await workspace.find({superior_id: id})
+            for (let item of items) {
+                descendants.push(item._id)
+
+                var stack = [];
+                stack.push(item);
+                workspaceitems.push(item)
+                while (stack.length > 0) {
+                    var currentnode = stack.pop();
+                    var children = await workspace.find({superior_id: currentnode._id});
+                    children.forEach(function (child) {
+                        descendants.push(child._id);
+                        workspaceitems.push(child)
+                        stack.push(child);
+                    });
+                }
+                descendants.join(",")
+                for (item of descendants) {
+                    await workspace.findByIdAndRemove(item.toString())
+
+                }
+
+
+            }
+            activeSession.deleteMany({userId: id})
+                .then((AS) => {
+                    console.log("ActiveSession deleted Succesufully")
+                    console.log(AS)
+                })
+
+
+
+
+
             user.password = undefined;
             let usertable = user
 
@@ -65,13 +60,14 @@ exports.DeleteUser=async (req, res) => {
 
             res.json({success: true, msg: "User has been deleted ", user: usertable});
 
-        } else {
+        }
+        else {
             res.json({success: false, msg: "error  user dosn't excite "});
         }
+    })}
 
 
-    })
-}
+
 
 
 
