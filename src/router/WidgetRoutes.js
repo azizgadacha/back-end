393f7fe338051e88938b81a67c34f78e5f9743c1
aziz gadacha <aziz.gadacha.2000@gmail.com>
2022-06-03 19:11:57 +0100

    remove old route

--- src/router/WidgetRoutes.js
+++ src/router/WidgetRoutes.js
@@ -36,22 +36,9 @@ const router = express.Router();
 const {lpm} = require("../config/lpm");
 
 //import  Data
-const {getData} = require("../function/functionData/getData");
 
 
 
-//import  Workspace
-const {shareWorkspace} = require("../function/functionWorkspace/shareWorkspace");
-const {getSharedWorkspace} = require("../function/functionWorkspace/getSharedWorkspaces");
-const {editworkspace} = require("../function/functionWorkspace/editworkspace");
-const {addinsideworkspace} = require("../function/functionWorkspace/addinsideworkspace");
-const {getworkspace} = require("../function/functionWorkspace/getworkspace");
-const {deleteworkspace}=require('../function/functionWorkspace/deleteworkspace');
-const {addworkspace}=require('../function/functionWorkspace/addworkspace')
-const {getinsideworkspace}=require('../function/functionWorkspace/getinsideworkspace')
-
-const {visualizationOfWorkspaces} = require("../function/functionWorkspace/visualizationOfWorkspaces");
-const {removeShare} = require("../function/functionWorkspace/removeShare");
 
 //import  Widget
 const {getWidget,
